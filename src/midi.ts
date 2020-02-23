b2444d9796701be1afcbe069481e9870e1971e54
Marvin Gaube <dev@marvingaube.de>
2020-02-23 17:23:51 +0100

    Initial Commit

---
+++ src/midi.ts
@@ -0,0 +1,8 @@
+// Import Utils
+import { Logging } from "@hibas123/nodelogging";
+// Import ShowComposer-Modules
+import { Datalib } from "@showcomposer/datalib";
+
+// Initialize globals
+// SC Data
+const data = new Datalib();
